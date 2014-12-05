f7febc7b99a4af1af610515de20b23b472391f7e
Anne-Mai Ilumäe <ami@ut.ee>
2014-12-06 01:30:47 +0200

    forloop

--- vcf_to_cg.py
+++ vcf_to_cg.py
@@ -8,6 +8,7 @@ This is made in python 2.7
 i=0
 linenr=0
 control_byone_li=[0]
+previous=None
 
 with open('GRC13292545_Ycap_rmdup_raw_v1.ann2.head1K.vcf','r') as filein, open(filein.name[:-4]+'.var', 'a') as fileout:
     fileout.write('#FILE_ID\t'+filein.name[:-4]+'\n')
@@ -15,17 +16,18 @@ with open('GRC13292545_Ycap_rmdup_raw_v1.ann2.head1K.vcf','r') as filein, open(f
     fileout.write('#GENERATED_BY_customscript\tvcf_to_cg.py\n')
     fileout.write('.\n')
     fileout.write('>locus\tploidy\tallele\tchromosome\tbegin\tend\tvarType\treference\talleleSeq\tvarScoreVAF\tvarScoreEAF\tvarQuality\thapLink\txRef\n')
-    for line in filein:
-        linenr+=1
+    for linenr, line in enumerate(filein, start=1):
+       # linenr+=1
         
         if line[:1]!='#': #or line[:1]!='#':
             i=i+1
             li=line.strip().split('\t')
-            print(filein.tell())
+                      
             nextline_li=next(filein).strip().split('\t')
             fileout.write(str(i)+'\t'+'1'+'\t'+'.\t'+'chrY\t'+li[1]+'\n')
             control_byone_li.append(li[1])
             control_byone_li.append(nextline_li[1])
+            previous=filein[linenr-1]
             
             print('*******'+li[1])
             print(control_byone_li[-1])
