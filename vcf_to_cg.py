a467c396b399f2c66224f4272fd1d969e02d01f4
Anne-Mai Ilumäe <ami@ut.ee>
2014-12-06 01:55:29 +0200

    readline_prev_curr

--- vcf_to_cg.py
+++ vcf_to_cg.py
@@ -8,23 +8,29 @@ This is made in python 2.7
 i=0
 linenr=0
 control_byone_li=[0]
-previous=None
+curr_line=" "
+prev_line=" "
+filein=open('GRC13292545_Ycap_rmdup_raw_v1.ann2.head1K.vcf','r') 
 
-with open('GRC13292545_Ycap_rmdup_raw_v1.ann2.head1K.vcf','r') as filein, open(filein.name[:-4]+'.var', 'a') as fileout:
+with open(filein.name[:-4]+'.var', 'a') as fileout:
     fileout.write('#FILE_ID\t'+filein.name[:-4]+'\n')
     fileout.write('#GENOME_REFERENCE\tNCBI build 37\n')
     fileout.write('#GENERATED_BY_customscript\tvcf_to_cg.py\n')
     fileout.write('.\n')
     fileout.write('>locus\tploidy\tallele\tchromosome\tbegin\tend\tvarType\treference\talleleSeq\tvarScoreVAF\tvarScoreEAF\tvarQuality\thapLink\txRef\n')
-    for linenr, line in enumerate(filein, start=1):
-       # linenr+=1
+    while (prev_line !=""):
+        curr_line=filein.readline()
         
-        if line[:1]!='#': #or line[:1]!='#':
+        if curr_line[:1]!='#':
+            continue#or line[:1]!='#':
+        else:
             i=i+1
-            li=line.strip().split('\t')
+            prev_line=curr_line
+            curr_line=filein.readline()
+            prev_li=prev_line.strip().split('\t')
+            curr_li=curr_line.strip().split('\t')
                       
-            nextline_li=next(filein).strip().split('\t')
-            fileout.write(str(i)+'\t'+'1'+'\t'+'.\t'+'chrY\t'+li[1]+'\n')
+            fileout.write(str(i)+'\t'+'1'+'\t'+'.\t'+'chrY\t'+prev_li[1]+'\n')
             control_byone_li.append(li[1])
             control_byone_li.append(nextline_li[1])
             previous=filein[linenr-1]
