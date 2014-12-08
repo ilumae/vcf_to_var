5e345d63569de44ef15de6eaf41ad789317253db
Anne-Mai Ilumäe <ami@ut.ee>
2014-12-08 17:03:48 +0200

    nextline

--- vcf_to_cg.py
+++ vcf_to_cg.py
@@ -1,13 +1,18 @@
-import itertools
+from itertools import tee, zip_longest
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
 
 This is made in python 2.7
 """
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip_longest(a, b)
 i=0
 linenr=0
-control_byone_li=[0]
+control_byone_li=[]
 
 with open('GRC13292545_Ycap_rmdup_raw_v1.ann2.head1K.vcf','r') as filein, open(filein.name[:-4]+'.var', 'a') as fileout:
     fileout.write('#FILE_ID\t'+filein.name[:-4]+'\n')
@@ -15,33 +20,35 @@ with open('GRC13292545_Ycap_rmdup_raw_v1.ann2.head1K.vcf','r') as filein, open(f
     fileout.write('#GENERATED_BY_customscript\tvcf_to_cg.py\n')
     fileout.write('.\n')
     fileout.write('>locus\tploidy\tallele\tchromosome\tbegin\tend\tvarType\treference\talleleSeq\tvarScoreVAF\tvarScoreEAF\tvarQuality\thapLink\txRef\n')
-    for line in filein:
+    for line, nextline in pairwise(filein):
         linenr+=1
         
         if line[:1]!='#': #or line[:1]!='#':
             i=i+1
             li=line.strip().split('\t')
-            print(filein.tell())
-            nextline_li=next(filein).strip().split('\t')
-            fileout.write(str(i)+'\t'+'1'+'\t'+'.\t'+'chrY\t'+li[1]+'\n')
-            control_byone_li.append(li[1])
-            control_byone_li.append(nextline_li[1])
+            if nextline==None:
+                break
+        
+            nextline_li=nextline.strip().split('\t')
+            fileout.write(str(i)+'\t'+'1'+'\t'+'.\t'+'chrY\t'+li[1]+nextline_li[1]+'\n')
             
-            print('*******'+li[1])
-            print(control_byone_li[-1])
-            print('!!!'+ str(int(control_byone_li[-1])-int(li[1])))
+            #control_byone_li.append(li[1])
+            #control_byone_li.append(nextline_li[1])
             
+            #print('*******'+li[1])
+            #print(control_byone_li[-1])
+            #print('!!!'+ str(int(control_byone_li[-1])-int(li[1])))
             
-            #if control_byone_li[-2]
-            #print int(li[1])-int(control_byone_li[-2])#==int(li[1])
+        
             
-            #if int(control_byone_li[-1])-int(li[1])!=1:
+            if li[6]=='PASS':
              #  print 'eurraaa'
-              # print int(control_byone_li[-1])-int(li[1])
-               #print linenr
-               #print line
+              #print(int(nextline_li[1])-int(li[1]))
+              print(linenr)
+              print(line)
               # break
     
-            if i==50:
-                break
+            #if i==50:
+#                print(control_byone_li)
+#                break
             
