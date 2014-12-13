ae55a0ee22f05a4e3ebd9465b1beabbcae93d3af
Anne-Mai Ilumäe <ami@ut.ee>
2014-12-13 14:36:33 +0200

    tartumoskva_diff=1lines

--- vcf_to_cg.py
+++ vcf_to_cg.py
@@ -16,7 +16,8 @@ def writqual(lis,filename):
         filename.write('CHECKCHEKCHECK\n')
         return 
             
-                        
+def vartype(lis, filename):
+
 
 
     
@@ -38,15 +39,15 @@ with open('GRC14392020_customer_variants.LF_moskva.vcf','r') as filein, open(fil
     fileout.write('.\n')
     fileout.write('>locus\tploidy\tallele\tchromosome\tbegin\tend\tvarType\treference\talleleSeq\tvarScoreVAF\tvarScoreEAF\tvarQuality\thapLink\txRef\n')
     for line, nextline in pairwise(filein):
-        if line==None:
+        if line=None:
             break
         linenr+=1
             
         
         if line[:1]!='#':
             i=i+1
-            if nextline==None:#misjuhtub faili l6puskontrollida??
-                break
+            #if nextline==None:#misjuhtub faili l6puskontrollida??
+#                break
             li=line.strip().split('\t')
             #nextline_li=nextline.strip().split('\t')
             #diff=int(nextline_li[1])-int(li[1])
@@ -65,58 +66,69 @@ with open('GRC14392020_customer_variants.LF_moskva.vcf','r') as filein, open(fil
                 
                     
                 firstline=False
-                temp=[li[1]]
+                temp= [li[1]]
                 continue
 
-            diff=int(li[1])-int(temp[0])
-                
         
-            if diff>1:
-                fileout.write(str(i)+'\t'+'1\t'+'1\t'+'chrY\t'+str(temp[0])+'\t'+str(int(li[1])-1)+'\t'+'no-call\t'+'=\t'+'?\n')
-                i=i+1
-                fileout.write(str(i)+'\t'+'1\t'+'1\t'+'chrY\t'+str(int(li[1])-1)+'\t'+str(li[1])+'\t')
-                if li[4]=='.':
-                    fileout.write('ref\t'+'=\t'+'=\t')
-                    writqual(li, fileout)
-                    temp=[li[1]]
-                    continue
+            else:
+                diff=int(li[1])-int(temp[0])
+                if diff>1:
+                    fileout.write(str(i)+'\t'+'1\t'+'1\t'+'chrY\t'+str(temp[0])+'\t'+str(int(li[1])-1)+'\t'+'no-call\t'+'=\t'+'?\n')
+                    i=i+1
+                    fileout.write(str(i)+'\t'+'1\t'+'1\t'+'chrY\t'+str(int(li[1])-1)+'\t'+str(li[1])+'\t')
+                    if li[4]=='.':
+                        fileout.write('ref\t'+'=\t'+'=\t')
+                        writqual(li, fileout)
+                        temp=[li[1]]
+                        continue
                         
                         
-                else:
-                    fileout.write('snp\t'+li[3]+'\t'+li[4]+'\t')
-                    writqual(li, fileout)
-                    temp=[li[1]]
-                    continue
-           if (diff==1):
-               fileout.write('APPI_1\n')
-                
-#            if diff==0:
-#                fileout.write('APPI_0\n')
-                
-                
-            
-                
-                 
-            
-            #control_byone_li.append(li[1])
-            #control_byone_li.append(nextline_li[1])
-            
-            #print('*******'+li[1])
-            #print(control_byone_li[-1])
-            #print('!!!'+ str(int(control_byone_li[-1])-int(li[1])))
-            
-        
-            
-            #if li[6]=='PASS':
+                    else:
+                        fileout.write('snp\t'+li[3]+'\t'+li[4]+'\t')
+                        writqual(li, fileout)
+                        temp=[li[1]]
+                        continue
+                if (diff==1):
+                    fileout.write(str(i)+'\t'+'1\t'+'1\t'+'chrY\t'+str(temp[0])+'\t'+str(int(li[1]))+'\t')
+                    if li[4]=='.':
+                        fileout.write('ref\t'+'=\t'+'=\t')
+                        writqual(li, fileout)
+                        temp=[li[1]]
+                        continue
+
+
+                    else:
+                        fileout.write('snp\t'+li[3]+'\t'+li[4]+'\t')
+                        writqual(li, fileout)
+                        temp=[li[1]]
+                        continue
+
+                #            if diff==0:
+                #                fileout.write('APPI_0\n')
+
+
+
+
+
+
+                #control_byone_li.append(li[1])
+                #control_byone_li.append(nextline_li[1])
+
+                #print('*******'+li[1])
+                #print(control_byone_li[-1])
+                #print('!!!'+ str(int(control_byone_li[-1])-int(li[1])))
+
+
+
+                #if li[6]=='PASS':
                 #fileout.write(li[8]
-            #else
-             #  print 'eurraaa'
-              #print(int(nextline_li[1])-int(li[1]))
-                  #print(linenr)
-                  #print(line)
-              # break
-    
-            #if i==50:
+                #else
+                #  print 'eurraaa'
+                #print(int(nextline_li[1])-int(li[1]))
+                #print(linenr)
+                #print(line)
+                # break
+
+                #if i==50:
 #                print(control_byone_li)
 #                break
-            
